d5307a5cffbcbee4fd0ec1066a47b9c051ec1afc
Magrid0 <magrid0@proton.me>
2024-06-04 18:30:13 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// Text
+string getChange = "How much money do you need to change? ";
+// Text end
+
+int coin[] = {500, 200, 100, 50, 20, 10, 5, 2, 1}; // Array with all the coin value
+
+void change(int amount) {
+}
+
+int main() {
+    cout << getChange;
+
+    return 0;
+}
